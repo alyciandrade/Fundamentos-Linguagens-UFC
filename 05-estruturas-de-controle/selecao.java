640662f8c3626ca3ee31900b8f999f070686db80
Alycia Andrade <132929970+alyciandrade@users.noreply.github.com>
2025-07-15 22:01:51 -0300

    Update selecao.java

--- 05-estruturas-de-controle/selecao.java
+++ 05-estruturas-de-controle/selecao.java
@@ -1,1 +1,19 @@
+ public class exemplo01 {
 
+      public static void main(String[] args) {
+      	      Scanner resposta = new Scanner (System.in);
+	
+    		    int idade;
+    		
+    		    System.out.print("Digite sua idade: ");
+    		    idade = resposta.nextInt();
+    		
+    		    if(idade < 18) {
+		                  System.out.println("O usuário é MENOR DE IDADE! ");
+	            }
+	            else 
+		                  System.out.println("O usuário é MAIOR DE IDADE! ");
+
+      }
+
+ }
