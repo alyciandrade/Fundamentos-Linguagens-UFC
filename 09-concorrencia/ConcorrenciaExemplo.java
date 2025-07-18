d4056408b02525ead61f0a7f25777f9cdf48c692
Alycia Andrade <132929970+alyciandrade@users.noreply.github.com>
2025-07-17 21:12:18 -0300

    Create ConcorrenciaExemplo.java

---
+++ 09-concorrencia/ConcorrenciaExemplo.java
@@ -0,0 +1,32 @@
+public class ConcorrenciaExemplo {
+
+    public static void main(String[] args) {
+        
+        Thread pares = new Thread(() -> {
+            for (int i = 0; i <= 15; i += 2) {
+                System.out.println("Pares: " + i);
+                try {
+                    Thread.sleep(500); 
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+       
+        Thread impares = new Thread(() -> {
+            for (int i = 1; i <= 15; i += 2) {
+                System.out.println("Ímpares: " + i);
+                try {
+                    Thread.sleep(500); 
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Inicia as duas threads
+        pares.start();
+        impares.start();
+    }
+}
